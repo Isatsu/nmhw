19fab88b4a0f8310d4461d4c98b3204119385d1e
Sikynat <caiquedadonas@outlook.com>
2020-10-15 16:17:55 -0300

    function to write on screen

--- script.js
+++ script.js
@@ -3,10 +3,12 @@
 function create(){
     var t1 = document.getElementById('txt1')
     var t2 = document.getElementById('title')
+    var t3 = document.getElementById('theme')
     var res  = document.getElementById('res')
-    
-    var nome = t1.value
+
+    var name = t1.value
     var title = t2.value
+    var theme = t3.value
     //Escreve na tela
-    res.innerHTML = `Seus dados são: ${nome} o titulo do seu projeto é ${title}`
+    res.innerHTML = `Seus dados são: ${name} o titulo do seu projeto é ${title} o tema é ${theme}`
 }
