b8c5604e35418354be91838bb481fa0ab52373d7
Sikynat <caiquedadonas@outlook.com>
2020-10-15 16:48:51 -0300

    function to create pdf

--- script.js
+++ script.js
@@ -6,9 +6,17 @@ function create(){
     var t3 = document.getElementById('theme')
     var res  = document.getElementById('res')
 
+    //Dados do usuario 
     var name = t1.value
     var title = t2.value
     var theme = t3.value
+
+    //Cria o pdf
+    var doc = new jsPDF()
+    doc.text(`Nome: ${name}`, 10, 10)
+    doc.text(`${title}`, 80, 20)
+    doc.save(`${title}.pdf`)
+
     //Escreve na tela
     res.innerHTML = `Seus dados são: ${name} o titulo do seu projeto é ${title} o tema é ${theme}`
 }
