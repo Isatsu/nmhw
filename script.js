afdffafe43a566a897a16b194008f8e5a3a2ffe5
Sikynat <caiquedadonas@outlook.com>
2020-10-15 16:13:51 -0300

    function to write on screen

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //Arquivo de scripts
+//Função que pega os dados do index html e escreve na tela
+function create(){
+    var t1 = document.getElementById('txt1')
+    var t2 = document.getElementById('title')
+    var res  = document.getElementById('res')
+    
+    var nome = t1.value
+    var title = t2.value
+    //Escreve na tela
+    res.innerHTML = `Seus dados são: ${nome} o titulo do seu projeto é ${title}`
+}
